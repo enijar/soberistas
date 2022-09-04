aaf2094de645d8149af86732aaa4006a25ae3e6c
Enijar <TheEnijar@gmail.com>
2022-09-04 17:02:22 +0100

    feat: add prisma

--- README.md
+++ README.md
@@ -7,5 +7,6 @@ A rewrite of the old [Soberistas code](https://bitbucket.org/FinerVision/soberis
 ```shell
 cp .env.example .env
 npm install
+npm run db-update
 npm start # http://localhost:3000
 ```
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,24 @@
 import type { GetServerSideProps, NextPage } from "next";
+import { User } from "prisma/prisma-client";
 import Header from "@/components/header/header";
+import database from "@/services/database";
 
 type Props = {
   title: string;
+  users: Partial<User>[];
 };
 
 export const getServerSideProps: GetServerSideProps<Props> = async () => {
+  const users = await database.user.findMany({
+    select: {
+      email: true,
+      password: true,
+    },
+  });
   return {
     props: {
       title: "Home",
+      users,
     },
   };
 };
@@ -18,6 +28,7 @@ const Home: NextPage<Props> = (props) => {
     <main>
       <Header />
       <h1>{props.title}</h1>
+      <pre>{JSON.stringify(props.users, null, 2)}</pre>
     </main>
   );
 };
